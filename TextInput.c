e5e5aaefa9a5833ca8c1f084ab648b8b27ac1b90
franciscosucre <franciscosucre@franciscosucre-C5500Q-C4505>
2015-05-08 14:22:13 -0430

    Se creo el algoritmo para leer la base de datos de los usuarios

--- TextInput.c
+++ TextInput.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include "ListaAmigos.h"
 
-ListaUsuarios *Leer_BaseDeDatos(FILE *files,ListaUsuarios *baseDatos)
+ListaUsuarios *Leer_BaseDeDatos(FILE *files,ListaUsuarios *base_Datos_Usuario)
 {
 
 	if (files == NULL)
@@ -18,5 +18,73 @@ ListaUsuarios *Leer_BaseDeDatos(FILE *files,ListaUsuarios *baseDatos)
 	   perror ("Error al abrir fichero.txt");
 	}
 
-	Usuario *AmigoNueva;
+	Usuario *usuario_Nuevo;
+	Amigo *amigo_Nuevo;
+	int comillas = 0;
+	char pieza[1];
+	char comilla[1] = {"\""};
+	char espacio = {" "};
+	char linea = {"\n"};
+	char verf_Linea;
+	int i = 0;
+
+	// Se leera la lista de usuarios
+
+	while (feof(files) == 0)
+	{
+		i = 0;
+		comillas = 0;
+		usuario_Nuevo = (Usuario *)malloc(sizeof(Usuario));
+		fscanf(files,"%s -> ",&usuario_Nuevo -> Nombre);
+		fflush(stdin);
+
+		// En este ciclo se leeran los amigos, leyendo un string y
+		// luego un caracter, el string se guarda y luego se verifica
+		// si el caracter es \n o " "
+
+		while (verf_Linea != linea)
+		{
+			amigo_Nuevo = (Amigo *)malloc(sizeof(Amigo));
+			fscanf(files,"%s",&amigo_Nuevo -> Nombre);
+			fflush(stdin);
+			fscanf(files,"%c",&verf_Linea);
+			fflush(stdin);
+
+			if (usuario_Nuevo -> primerAmigo == NULL)
+			{
+				usuario_Nuevo -> primerAmigo = amigo_Nuevo;
+				usuario_Nuevo -> ultimoAmigo = amigo_Nuevo;
+			}
+
+			else
+			{
+				amigo_Nuevo -> anterior = usuario_Nuevo ->ultimoAmigo;
+				usuario_Nuevo -> ultimoAmigo -> siguiente =
+						amigo_Nuevo;
+				usuario_Nuevo -> ultimoAmigo = amigo_Nuevo;
+
+			}
+
+		}
+
+		if (base_Datos_Usuario -> primerUsuario == NULL)
+		{
+			base_Datos_Usuario -> primerUsuario = usuario_Nuevo;
+			base_Datos_Usuario -> ultimoUsuario = usuario_Nuevo;
+		}
+
+		else
+		{
+			usuario_Nuevo -> anterior =
+					base_Datos_Usuario -> ultimoUsuario;
+			base_Datos_Usuario -> ultimoUsuario ->siguiente =
+					usuario_Nuevo;
+			base_Datos_Usuario -> ultimoUsuario = usuario_Nuevo;
+
+
+		}
+		return base_Datos_Usuario;
+
+	}
+
 }
