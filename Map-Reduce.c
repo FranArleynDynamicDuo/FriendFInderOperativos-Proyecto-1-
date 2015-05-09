fb0823b4005bbe3f69251cbdd1ee68a0686a3dbe
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-09 18:00:33 -0430

    Commit Arleyn

--- Map-Reduce.c
+++ Map-Reduce.c
@@ -5,4 +5,51 @@
  *      Author: franciscosucre
  */
 
+#include <stdlib.h>
+#include "Map-Reduce.h"
+#include "ListaAmigos.h"
+
+ListaMap *Mapeo(Usuario *base_Datos_Usuario) /*base_Datos_Usuarios Nombre de la listas de listas*/
+
+{
+	Usuario *aux;
+	Amigo *auxx;
+	Amigo *auxxx;
+
+	Map *auxMap;
+	aux = base_Datos_Usuario->primerUsuario;
+
+
+	while (aux != NULL)
+
+	{
+
+		auxx = aux->primerAmigo;
+		while (auxx != NULL)
+
+		{
+			Map *nuevo = (Map *)malloc(sizeof(Map));
+			auxMap = nuevo;
+			auxxx = aux->primerAmigo;
+			nuevo->UsuarioMap = aux->Nombre;
+			nuevo->AmigoMap = auxx->Nombre;
+
+			while (auxxx != NULL)
+			{
+				amigoM *new  = (new *)malloc(sizeof(new));
+				new->AmigoM = auxxx->Nombre;
+				auxMap->siguiente= new;
+				auxMap = new;
+				auxxx = auxxx->siguiente;
+				if (auxxx == NULL)
+				{
+					auxx = auxx->siguiente;
+				}
+			}
+
+		}
+
+		aux = aux->siguiente;
+	}
+
 
--- Map-Reduce.h
+++ Map-Reduce.h
@@ -8,6 +8,32 @@
 #ifndef MAP_REDUCE_H_
 #define MAP_REDUCE_H_
 
+typedef struct map
+
+{
+	char UsuarioMap[30];
+	char AmigoMap[30];
+	struct AmigoM *siguiente;
+	struct AmigoM *anterior;
+
+} Map
+
+typedef struct amigoM
+
+{
+	char AmigoM[30];
+	struct Amigo *siguiente;
+	struct Amigo *anterior;
+
+} amigoM;
+
+typedef struct listaMap
+
+{
+	Map *primero;
+	Map *ultimo;
+
+} ListaMap;
 
 
 #endif /* MAP_REDUCE_H_ */
