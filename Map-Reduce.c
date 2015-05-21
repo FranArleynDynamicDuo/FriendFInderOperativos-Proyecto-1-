2e6ae5556e51c317001ba0cdb09bc8ad4fa6799b
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-20 23:54:01 -0430

    ACTUALIZACION DEL MAP

--- Map-Reduce.c
+++ Map-Reduce.c
@@ -4,6 +4,18 @@
  *  Created on: May 5, 2015
  *      Author: franciscosucre
  */
+/*
+ * Nuevo.c
+ *
+ *  Created on: 20/5/2015
+ *      Author: personal
+ */
+/*
+ * Map-Reduce.c
+ *
+ *  Created on: May 5, 2015
+ *      Author: franciscosucre
+ */
 
 #include <stdlib.h>
 #include "Map-Reduce.h"
@@ -27,6 +39,12 @@ ListaMap *Mapeo(ListaUsuarios *base_Datos_Usuario,
 	Map *aux_Comp;
 	amigoM *new_Friend;
 
+	int sem;
+
+	char usuario_Comp[100];
+	char nombre_MapAct[100];
+	char amigo_MapAct[100];
+	char amigo_Comp[100];
 
 	aux_Usuario = base_Datos_Usuario-> primerUsuario;
 
@@ -37,54 +55,80 @@ ListaMap *Mapeo(ListaUsuarios *base_Datos_Usuario,
 		while (aux_Amigo != NULL)
 		{
 			nuevo_Map = (Map *)malloc(sizeof(Map));
-			nuevo_Map ->siguiente = NULL;
-			nuevo_Map ->primer_Amigo_Map = NULL;
-			nuevo_Map ->ultimo_Amigo_Map = NULL;
+			nuevo_Map -> siguiente = NULL;
+			nuevo_Map -> primer_Amigo_Map = NULL;
+			nuevo_Map -> ultimo_Amigo_Map = NULL;
+			sem = 0;
 
 
 			if(base_Mapeada->primero != NULL)
 			{
 				aux_Comp = (Map *)malloc(sizeof(Map));
 				aux_Comp = base_Mapeada -> primero;
-				printf("aux_Comp: %s",aux_Comp ->UsuarioMap );
 
-				while((strcmp(aux_Comp -> UsuarioMap,
-						aux_Usuario -> Nombre)))
+				strcpy(usuario_Comp,aux_Comp -> UsuarioMap);
+				strcpy(amigo_Comp,aux_Comp -> AmigoMap);
+				strcpy(nombre_MapAct,aux_Usuario -> Nombre);
+				strcpy(amigo_MapAct,aux_Amigo -> Nombre);
+//
+//				printf("usuario_Comp: %s \n",usuario_Comp);
+//				printf("amigo_Comp: %s \n",amigo_Comp);
+//				printf("nombre_MapAct: %s \n",nombre_MapAct);
+//				printf("amigo_MapAct: %s \n\n",amigo_MapAct);
+
+
+
+				while((strcmp(usuario_Comp,nombre_MapAct) != 0) &&
+						(aux_Comp -> siguiente != NULL) )
 
 				{
-					if ((((strcmp(aux_Comp -> UsuarioMap,
-							aux_Amigo -> Nombre)) == 0)
-							&&
-							((strcmp(aux_Comp ->AmigoMap,
-									aux_Usuario ->Nombre)) == 0)))
-					{
+//					printf("usuario_Comp: %s \n",usuario_Comp);
+//					printf("amigo_Comp: %s \n",amigo_Comp);
+//					printf("nombre_MapAct: %s \n",nombre_MapAct);
+//					printf("amigo_MapAct: %s \n\n",amigo_MapAct);
 
-						while(aux_Amigo != NULL)
+					if ((((strcmp(nombre_MapAct,amigo_Comp)) == 0)
+							&&
+							((strcmp(usuario_Comp,amigo_MapAct)) == 0)))
+					{
+						aux_Amigo_De_Amigo = aux_Usuario->primerAmigo;
+
+						while(aux_Amigo_De_Amigo != NULL)
 						{
 							new_Friend  = (amigoM *)malloc(
 									sizeof(amigoM));
 							new_Friend ->siguiente = NULL;
 
-							new_Friend ->AmigoM = aux_Amigo->Nombre;
+							new_Friend ->AmigoM =
+									aux_Amigo_De_Amigo->Nombre;
 							new_Friend->anterior =
 									aux_Comp -> ultimo_Amigo_Map;
 							aux_Comp ->ultimo_Amigo_Map ->siguiente =
 									new_Friend;
 							aux_Comp ->ultimo_Amigo_Map = new_Friend;
-							aux_Amigo = aux_Amigo -> siguiente;
+							aux_Amigo_De_Amigo =
+									aux_Amigo_De_Amigo -> siguiente;
+							sem = 1;
 						}
+						aux_Amigo = aux_Amigo -> siguiente;
 						break;
 					}
-					aux_Comp = aux_Comp ->siguiente;
+
+					aux_Comp = aux_Comp -> siguiente;
+					strcpy(usuario_Comp,aux_Comp -> UsuarioMap);
+
 				}
 			}
-			//auxMap = nuevo;
 
-			aux_Amigo_De_Amigo = aux_Usuario->primerAmigo;
 			nuevo_Map->UsuarioMap = aux_Usuario->Nombre;
 			nuevo_Map->AmigoMap = aux_Amigo->Nombre;
 
-			while (aux_Amigo_De_Amigo != NULL)
+			if (sem == 0)
+			{
+				aux_Amigo_De_Amigo = aux_Usuario->primerAmigo;
+			}
+
+			while ((aux_Amigo_De_Amigo != NULL) && (sem = 0))
 			{
 				new_Friend  = (amigoM *)malloc(sizeof(amigoM));
 				new_Friend->AmigoM = aux_Amigo_De_Amigo->Nombre;
@@ -127,6 +171,11 @@ ListaMap *Mapeo(ListaUsuarios *base_Datos_Usuario,
 								nuevo_Map;
 						base_Mapeada -> ultimo = nuevo_Map;
 					}
+
+			if (sem == 1)
+			{
+				aux_Amigo = aux_Amigo->siguiente;
+			}
 		}
 		aux_Usuario = aux_Usuario->siguiente;
 	}
